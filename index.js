37a525cc13fbab1a90965d33d47b347ace04c903
Marya A <maria00@gmail.com>
2022-07-06 22:55:43 +0300

    Commiting HTML Files

---
+++ index.js
@@ -0,0 +1,10 @@
+function addNumbers(){
+    var num1 = Number(document.getElementById('first').value)
+    var num2 = Number(doument.getElementById('second').value)
+    var sum = num1 + num2;
+    console.log('The sume is: ',sum)
+    
+   
+}
+addNumbers();
+//console.log('Hi from external file')
---
+++ welcome.js
@@ -0,0 +1,12 @@
+function addNumbers(){
+
+
+var firstNumber =10;
+var secondNumber =20;
+
+console.log(typeof(secondNumber))
+var sum = firstNumber + secondNumber
+
+console.log('The sume is: ', sum);
+}
+addNumbers();
